d23cacb43126b19ddb75213915c95b360948c948
chang-ryan <ry4nch4ng@gmail.com>
2015-11-20 15:39:39 -0800

    javascript calculator first commit

---
+++ calculator.js
@@ -0,0 +1,90 @@
+var num1 = [];
+var num2 = [];
+var operand = null;
+var operands = /[\+\-\/x=]/
+
+// Calculator Functions
+
+function add(n1, n2) {
+  var result = n1 + n2;
+  return result;
+};
+function multiply(n1,n2) {
+  var result = n1 * n2;
+  return result;
+};
+function clear() {
+  num1 = [];
+  num2 = [];
+  operand = null;
+  $("#screen").html("0")
+};
+function numberize(array) {
+  return +array.join('')
+};
+
+$(document).ready(function() {
+  // Start
+
+  $(".key").click(function() {
+    var input = $(this).html();
+
+    // c pressed
+    if (/c/i.test(input)) {
+      clear();
+    }
+
+    // numbers pressed
+    if (!operand && $(this).hasClass("num")) {
+      if (input === ".") {
+        if (num1.indexOf(".") === -1) {
+          num1.push(input);
+        }
+      } else {
+        num1.push(input);
+      }
+      $("#screen").html(num1);
+    } else if ($(this).hasClass("num")) {
+      if (input === ".") {
+        if (num2.indexOf(".") === -1) {
+          num2.push(input);
+        }
+      } else {
+        num2.push(input);
+      }
+      $("#screen").html(num2);
+    }
+
+    // operands pressed
+    if (operands.test(input) && num1.length > 0) {
+      if (num2.length > 0) {
+        var n1 = numberize(num1);
+        var n2 = numberize(num2);
+        result = null;
+
+        switch (operand) {
+          case '+':
+            result = add(n1,n2);
+            break;
+          case '-':
+            result = n1 - n2;
+            break;
+          case 'x':
+            result = multiply(n1,n2);
+            break;
+          case '/':
+            result = n1 / n2;
+            break;
+          case '=':
+            break;
+        }
+        num1 = result.toString().split('');
+        num2 = [];
+        $("#screen").html(num1);
+      }
+      operand = input;
+    }
+  });
+
+  // End
+});
